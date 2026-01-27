1c6f40f32007fbd5bb3033a4402a4b7dad70877c
EmmanuelKibe <emmanuelkibe148@gmail.com>
2026-01-27 13:20:22 +0300

    Create views for the serializers

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Vehicle, Shipment
+from .serializers import VehicleSerializer, ShipmentSerializer
 
-# Create your views here.
+class VehicleList(generics.ListCreateAPIView):
+    queryset = Vehicle.objects.all()
+    serializer_class = VehicleSerializer
+
+class ShipmentList(generics.ListCreateAPIView):
+    queryset = Shipment.objects.all()
+    serializer_class = ShipmentSerializer
