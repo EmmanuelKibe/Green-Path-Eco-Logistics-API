69531cc8d8dd3a553efae214a3ed4f43a4d5c263
EmmanuelKibe <emmanuelkibe148@gmail.com>
2026-01-28 12:26:31 +0300

    Create API documentation using Swagger

--- green_path/urls.py
+++ green_path/urls.py
@@ -16,8 +16,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Green-Path Ecologistics API",
+      default_version='v1',
+      description="API for calculating and tracking shipment carbon footprints",
+      contact=openapi.Contact(email="contact@greenpath.local"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
