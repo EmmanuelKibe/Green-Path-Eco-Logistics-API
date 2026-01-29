4f0cd36eae695475f2aa66c5c540485aa63348d7
EmmanuelKibe <emmanuelkibe148@gmail.com>
2026-01-29 15:37:36 +0300

    Add permission classes to the serializers to prevent access and modifications by users not logged in.  Assign each shipment to the user who created it

--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, filters
+from rest_framework import generics, filters, permissions
 from .models import Vehicle, Shipment
 from .serializers import VehicleSerializer, ShipmentSerializer
 from django_filters.rest_framework import DjangoFilterBackend
@@ -7,10 +7,11 @@ from django_filters.rest_framework import DjangoFilterBackend
 class VehicleList(generics.ListCreateAPIView):
     queryset = Vehicle.objects.all()
     serializer_class = VehicleSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly] #Can be seen publicly but only modified by staff
 
 class ShipmentList(generics.ListCreateAPIView):
-    queryset = Shipment.objects.all()
     serializer_class = ShipmentSerializer
+    permission_classes = [permissions.IsAuthenticated] #Must be logged in to do anything
     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     
     # Enable filtering by specific fields
@@ -21,3 +22,11 @@ class ShipmentList(generics.ListCreateAPIView):
     
     # Allow users to order results by date or carbon footprint
     ordering_fields = ['created_at', 'carbon_footprint', 'weight']
+
+    def get_queryset(self):
+        # Users only see THEIR own shipments
+        return Shipment.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        # Automatically set the owner to the current logged-in user
+        serializer.save(owner=self.request.user)
