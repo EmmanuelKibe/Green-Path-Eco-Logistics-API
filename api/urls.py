17e9f61afb17a40c20f0468914e70ca5e6886e9c
EmmanuelKibe <emmanuelkibe148@gmail.com>
2026-01-29 16:23:22 +0300

    Route the shipmentdetail view in urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import VehicleList, ShipmentList
+from .views import VehicleList, ShipmentList, ShipmentDetail
 
 urlpatterns = [
     path('vehicles/', VehicleList.as_view(), name='vehicle-list'),
     path('shipments/', ShipmentList.as_view(), name='shipment-list'),
+    path('shipments/<uuid:pk>/', ShipmentDetail.as_view(), name='shipment-detail'),
 ]
