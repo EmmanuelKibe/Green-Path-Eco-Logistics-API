8a81b1342ba1c6aa2efb8b4315d6ecf8701a3065
EmmanuelKibe <emmanuelkibe148@gmail.com>
2026-02-03 21:44:11 +0300

    Remove redundant files

--- green_path/settings.py
+++ green_path/settings.py
@@ -25,9 +25,6 @@ Env.read_env(os.path.join(BASE_DIR, '.env'))
 # Set the project base directory
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Take environment variables from .env file
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
