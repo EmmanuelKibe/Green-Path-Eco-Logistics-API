530639681b1d42e723b54b076afd621a8f6fabb7
EmmanuelKibe <emmanuelkibe148@gmail.com>
2026-02-03 14:08:30 +0300

    Update Allowed Hosts

--- green_path/settings.py
+++ green_path/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'green-path-eco-logistics-api-1.onrender.com']
 
 render_host = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if render_host:
